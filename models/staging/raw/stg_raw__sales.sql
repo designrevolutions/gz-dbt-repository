633dd0a646b4df6e26275331eb962e7c29543f44
omar@designrevolutions.com <omar@designrevolutions.com>
2024-09-11 10:35:59 +0000

    Wed 11 Sep 2024, 1135 Carrying on from yesterday

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -2,16 +2,21 @@ with
 
 source as (
 
-    select * from {{ source('raw', 'sales') }}
+    select 
+        *
+        -- CONCAT(date_date, '-', orders_id) AS unique_id
+
+    from {{ source('raw', 'sales') }}
 
 ),
 
 renamed as (
 
     select
+        -- unique_id,
         date_date,
         orders_id,
-        pdt_id,
+        pdt_id AS products_id,
         revenue,
         quantity
 
